f202a7279be28d2cd517358689f2b7002a4dc5b4
gh-actions <actions@github.com>
2022-02-23 02:10:09 +0000

    Deploy website - based on c287167a359b90ea30c87537c450d7929074457d

---
+++ assets/js/c2c083ff.655ff3de.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwarewulf=self.webpackChunkwarewulf||[]).push([[713],{5641:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"4.0.0","permalink":"/news/4.0.0"}]}')}}]);
